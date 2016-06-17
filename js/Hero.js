35a302cb3777d19d1cadc549c0fe7434b7482877
steviebarrett <mail@steviebarrett.com>
2016-06-17 17:32:38 +0100

    Added Touch handling classes and some testing code

--- js/Hero.js
+++ js/Hero.js
@@ -6,11 +6,29 @@ function Hero(sprite, position) {
 
 Hero.prototype = Object.create(Character.prototype);
 
+Object.defineProperty(Hero.prototype, "heroSelectRectangle",
+	{
+		get: function () {
+			return new Rectangle(this.position.x - this.origin.x, this.position.y - this.origin.y,
+			sprites.hero.height, sprites.hero.height);
+		}
+	});
+
 Hero.prototype.handleInput = function (delta) {
 
 	var newX = this.position.x;
 	var newY = this.position.y;
 
+	if (Touch.isTouchDevice) {
+		var rect = this.heroSelectRectangle;
+		if (Touch.touching && !Touch.containsTouch(rect)) {
+			newX = Touch.position.x;
+			newY = Touch.position.y;
+		} else if (Touch.containsTouchPress(rect)) {
+			//use this to detect if press inside the rectangle
+		}
+	}
+	
 	//TODO: Refactor keyboard handling into better code
 	if (Keyboard.down(Keys.up)) {
 		newY -= 10;
