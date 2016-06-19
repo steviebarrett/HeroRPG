f2e3af7ef94973c7de86cdf824c9575a2ec6a8c2
steviebarrett <mail@steviebarrett.com>
2016-06-19 16:44:14 +0100

    Began basic code to create grid structure in HeroGameWorld.js

--- js/HeroGameWorld.js
+++ js/HeroGameWorld.js
@@ -3,7 +3,25 @@
 function HeroGameWorld() {
 	this.hero = new Hero(sprites.hero, new Vector2(72, 405));
 	this.enemy = new Enemy(sprites.enemy, new Vector2(200, 40));
+
+	//the grid array
+	this.rows = 5;
+	this.cold = 10;
+	this.grid = new Array(this.rows * this.cols);
+	for (var i = 0;i < this.rows * this.cols; i++) {
+		this.grid[i] = sprites.hero;    //fill the grid with heroes just now
+	}
 }
+
+HeroGameWorld.prototype.setGridValue = function (x, y, value) {
+	var index = y * this.columns + x;
+	this.grid[index] = value;
+};
+
+HeroGameWorld.prototype.getGridValue = function (x, y) {
+	var index = y * this.columns + x;
+	return this.grid[index];
+};
 
 HeroGameWorld.prototype.handleInput = function (delta) {
 	this.hero.handleInput(delta);
